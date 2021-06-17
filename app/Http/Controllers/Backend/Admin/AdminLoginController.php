6cb3e38bb09bcaa200ed9df787454617c004e0b9
DuongThuyNgo <duongthuyngo2k@gmail.com>
2021-06-17 21:38:25 +0700

    Added Laravel

---
+++ app/Http/Controllers/Backend/Admin/AdminLoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdminLoginController extends Controller
+{
+    public function getLoginAdmin(){
+        return view('backend.admin.login');
+    }
+
+    public function postLoginAdmin(Request $request){
+        $credentials=$request->only('email','password');
+        //dd($credentialss);
+        if(Auth::guard('admins')->attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->route('get_backend.home');
+        }
+        return redirect()->back();
+    }
+    public function getLogout(Request $request)
+    {
+
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('get_backend.login');
+    }
+}
